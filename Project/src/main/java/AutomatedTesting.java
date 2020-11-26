d491586b799dd1ccb95a5fd2f2abe6bad32ad578
181250034 <181250034@smail.nju.edu.cn>
2020-11-26 20:32:23 +0800

    [lalla]

--- Project/src/main/java/AutomatedTesting.java
+++ Project/src/main/java/AutomatedTesting.java
@@ -28,8 +28,8 @@ public class AutomatedTesting {
     private final HashSet<String> allListedMethods = new HashSet<String>();  //listed method
     private final HashMap<String, List<String>> callerRelation = new HashMap<String, List<String>>(); // key为方法，value为所有直接调用了key方法的方法
     private final ArrayList<String> changeInfo = new ArrayList<String>(); // 记录所有的变更信息
-    private final HashMap<String, List<String>> classDependencies = new HashMap<>();  // 类级别依赖信息，key为类名，value为所有直接依赖该类的类名
-    private final HashMap<String, List<String>> methodDependencies = new HashMap<>(); // 方法级别依赖，key为方法名，value为所有直接依赖该方法的方法名
+    public final HashMap<String, List<String>> classDependencies = new HashMap<>();  // 类级别依赖信息，key为类名，value为所有直接依赖该类的类名
+    public final HashMap<String, List<String>> methodDependencies = new HashMap<>(); // 方法级别依赖，key为方法名，value为所有直接依赖该方法的方法名
     private final String infoPath;
     private final String endPath;
     private final String rootPath = "D:\\AutomatedTesting\\Project\\src\\main\\resources\\scope.txt";
@@ -38,77 +38,36 @@ public class AutomatedTesting {
         this.endPath = endPath;
         this.infoPath = infoPath;
     }
-    public void getallFindedClasses(CHACallGraph cg) {
-        for (CGNode node : cg) {
-            if (node.getMethod() instanceof ShrikeBTMethod) {
-                ShrikeBTMethod method = (ShrikeBTMethod) node.getMethod();
-                if ("Application".equals(method.getDeclaringClass().getClassLoader().toString())) {
-                    String classInnerName = method.getDeclaringClass().getName().toString();
-                    this.allFindedClasses.add(classInnerName.split("\\$")[0]);
-                }
-            }
-        }
-    }
 
-    //class TCS
-    public void selectByClass() {
-        /**
-         * 先找到所有有关联的类，然后去allListedMethods中找到那些对应的方法
-         */
-        HashSet<String> relatedClasses = new HashSet<String>();
-        HashSet<String> selectedMethods = new HashSet<String>();
-        for (String s : changeInfo) {
-            String searchClass = s.split(" ")[0];
-            relatedClasses.add(searchClass);
-            selectByClass_r(relatedClasses, searchClass);
-        }
-
-        for (String s : allListedMethods) {
-            if (relatedClasses.contains(s.split(" ")[0])) {
-                selectedMethods.add(s);
-            }
-        }
-        this.storeSelectedMethods(selectedMethods, "./selection-class.txt");
-    }
-
-    public void selectByClass_r(HashSet<String> relatedClasses, String searchClass) {
+    //main recursive select by class
+    public void byClass(HashSet<String> relatedClasses, String searchClass) {
         for (String callee : callerRelation.keySet()) {
             if (callee.split(" ")[0].equals(searchClass)) {
                 for (String caller : callerRelation.get(callee)) {
                     relatedClasses.add(caller.split(" ")[0]);
                     if (relatedClasses.contains(caller.split(" ")[0]))
                         continue;
-                    // recursive to find caller related class，find间接依赖
-                    selectByClass_r(relatedClasses, caller.split(" ")[0]);
+                    // recursive to find caller related class，find
+                    byClass(relatedClasses, caller.split(" ")[0]);
                 }
             }
         }
     }
-
-    // method TCS
-
-    public void selectByMethod() {
-        HashSet<String> selectedMethods = new HashSet<String>();
-        for (String s : changeInfo) {
-            HashSet<String> relatedMethods = new HashSet<String>();
-            selectByMethod_r(relatedMethods, s);
-            selectedMethods.addAll(relatedMethods);
-        }
-        this.storeSelectedMethods(selectedMethods, "./selection-method.txt");
-    }
-
-    //gather set of TC to a fileWriter
-    public void storeSelectedMethods(HashSet<String> selectedMethods, String filename) {
+    //gather set of TestCase to a fileWriter
+    public void methodsStoring(HashSet<String> selectedMethods, String filename) {
         try {
+            int count = 0;
             File file = new File(filename);
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             for (String string : selectedMethods) {
-                // 只选择测试方法
-                if (testListedMethods.contains(string))
+                if (testListedMethods.contains(string)){
                     bufferedWriter.write(string + "\n");
+                    count ++;
+                }
             }
             bufferedWriter.flush();
+            System.out.print(count);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -132,10 +91,26 @@ public class AutomatedTesting {
                 }
             }
         }
-        this.storeMethodDependDotFile("./method-dependencies.dot");
+        try {
+            File file = new File("./method-dependencies.dot");
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("digraph dependencies {\n");
+            for (String string : methodDependencies.keySet()) {
+                for (String s : methodDependencies.get(string)) {
+                    bufferedWriter.write("\t");
+                    bufferedWriter.write("\"" + string + "\" -> \"" + s + "\";\n");
+                }
+            }
+            bufferedWriter.write("}");
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void selectByMethod_r(HashSet<String> relatedMethod, String searchMethod) {
+    public void byMethod(HashSet<String> relatedMethod, String searchMethod) {
         HashSet<String> newMethod = new HashSet<String>();
         if (!this.callerRelation.containsKey(searchMethod))
             return;
@@ -146,7 +121,7 @@ public class AutomatedTesting {
             }
         }
         for (String s : newMethod) {
-            selectByMethod_r(relatedMethod, s);
+            byMethod(relatedMethod, s);
         }
     }
 
@@ -185,16 +160,15 @@ public class AutomatedTesting {
             }
         }
 
-        System.out.println("show call relation");
+        System.out.println("Well, show call relation");
         for (String callee : callerRelation.keySet()) {
             System.out.println(callee);
-            System.out.println("called by:");
             for (String s : callerRelation.get(callee)) {
                 System.out.println(s);
             }
             System.out.println();
         }
-        //将callerRelation中的所有方法拿出来放到allListedMethods中
+        //All the methods of callerRelation to allListedMethods
         for (String callee : callerRelation.keySet()) {
             allListedMethods.add(callee);
             allListedMethods.addAll(callerRelation.get(callee));
@@ -211,7 +185,7 @@ public class AutomatedTesting {
             while ((line = reader.readLine()) != null) {
                 tmp = line.split(" ");
                 if (tmp.length != 2) {
-                    System.out.println("change_info.txt format is wrong");
+                    System.out.println("wrong with change_info.txt format");
                     break;
                 }
                 //索引0存放类，索引1存放方法
@@ -251,36 +225,13 @@ public class AutomatedTesting {
                 }
             }
         }
-        this.storeClassDependDotFile("./class-dependencies.dot");
-    }
-
-    public void storeClassDependDotFile(String filename) {
         try {
-            File file = new File(filename);
+            File file = new File("./class-dependencies.dot");
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write("digraph dependencies {\n");
             for (String string : classDependencies.keySet()) {
                 for (String s : classDependencies.get(string)) {
-                    bufferedWriter.write("\t");
-                    bufferedWriter.write("\"" + string + "\" -> \"" + s + "\";\n");
-                }
-            }
-            bufferedWriter.write("}");
-            bufferedWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void storeMethodDependDotFile(String filename) {
-        try {
-            File file = new File(filename);
-            FileWriter fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("digraph dependencies {\n");
-            for (String string : methodDependencies.keySet()) {
-                for (String s : methodDependencies.get(string)) {
                     bufferedWriter.write("\t");
                     bufferedWriter.write("\"" + string + "\" -> \"" + s + "\";\n");
                 }
@@ -311,12 +262,19 @@ public class AutomatedTesting {
                 scope.addClassFileToScope(ClassLoaderReference.Application, f);
             }
         }
-
         ClassHierarchy cha = ClassHierarchyFactory.makeWithRoot(scope);
         Iterable<Entrypoint> eps = new AllApplicationEntrypoints(scope, cha);
         CHACallGraph cg = new CHACallGraph(cha);
         cg.init(eps);
-        this.getallFindedClasses(cg);
+        for (CGNode node : cg) {
+            if (node.getMethod() instanceof ShrikeBTMethod) {
+                ShrikeBTMethod method = (ShrikeBTMethod) node.getMethod();
+                if ("Application".equals(method.getDeclaringClass().getClassLoader().toString())) {
+                    String classInnerName = method.getDeclaringClass().getName().toString();
+                    this.allFindedClasses.add(classInnerName.split("\\$")[0]);
+                }
+            }
+        }
         this.buildcallerRelation(cg);
         try {
             BufferedReader in = new BufferedReader(new FileReader(this.infoPath));
@@ -327,15 +285,35 @@ public class AutomatedTesting {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        //factory c or m
         if (selectOption == 'c') {
-            this.selectByClass();
+            HashSet<String> relatedClasses = new HashSet<String>();
+            HashSet<String> selectedMethods = new HashSet<String>();
+            for (String s : changeInfo) {
+                String searchClass = s.split(" ")[0];
+                relatedClasses.add(searchClass);
+                byClass(relatedClasses, searchClass);
+            }
+
+            for (String s : allListedMethods) {
+                if (relatedClasses.contains(s.split(" ")[0])) {
+                    selectedMethods.add(s);
+                }
+            }
+            this.methodsStoring(selectedMethods, "./selection-class.txt");
         } else if (selectOption == 'm') {
-            this.selectByMethod();
+            HashSet<String> selectedMethods = new HashSet<String>();
+            for (String s : changeInfo) {
+                HashSet<String> relatedMethods = new HashSet<String>();
+                byMethod(relatedMethods, s);
+                selectedMethods.addAll(relatedMethods);
+            }
+            this.methodsStoring(selectedMethods, "./selection-method.txt");
         } else {
             System.err.println("Unknown select option" + selectOption);
         }
         this.getClassDependencies();
         this.getMethodDependencies();
     }
+
 }
